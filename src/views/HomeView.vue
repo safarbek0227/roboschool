3df72623cf2041307614726ff975b7c524555a2c
Safarbek <solijonovsafarbek@gmail.com>
2022-08-05 03:23:42 -0700

    new changes
    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,13 @@
 
  <transition name="fade">
   <div class="home container page">
-    <h1><span class="text-gradient">RoboSchool</span></h1>
     <img src="@/assets/fixing-robot.png" alt="">
+    <h1>Bizning<span class="text-gradient">RoboSchool</span>ga xush kelibsiz</h1>
+    <h2>
+      Bizning saytda <strong>Arduino</strong> va <strong>robtotexnikani</strong> onlayn o'rganing
+      <br>
+      O'zingizda kerakli buyumlarni bizning <a href="/shop"> sotuv bolimimizdan</a> sotib olishiz mumkin
+    </h2>
   </div>
 </transition>
 
@@ -14,9 +19,10 @@
   margin-top: 50px;
 }
 img{
-  width: 40%;
+  width: 300px;
 }
 h1{
   font-size: 3em;
+  font-weight:500;
 }
 </style>
