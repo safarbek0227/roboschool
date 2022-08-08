4be94f83fe9d4f5c3821c29b0a241f8642c4eadb
Safarbek <solijonovsafarbek@gmail.com>
2022-08-08 09:45:10 -0700

    .

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,4 @@
 <template>
-        <vue-headful
-            title="Title from vue-headful"
-            description="Description from vue-headful"
-            image='@/assets/fixing-robot.png'
-        />
   <RouterView />
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import vueHeadful from 'vue-headful';
 
 const app = createApp(App)
 
 app.use(router)
-app.component('vue-headful', vueHeadful);
 app.mount('#app')
 
